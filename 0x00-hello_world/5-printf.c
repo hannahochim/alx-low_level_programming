19ef6fcba2742dc1d41b1681b16752484f8a59e1
hannahochim <hannahochim@gmail.com>
2023-10-09 21:58:43 -0700

    Update files

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Printf function
+ * Description: Using main, this code prints a string with the printf function
+ *
+ * Return: returns 0 on success
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
