3bc7f2df8a7934dcee042708aa5eefc5037c0c13
hannahochim <hannahochim@gmail.com>
2024-01-27 23:52:06 -0800

    Print if 0, positive or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/** Author: Hannah L. Ochim
+ *
+ * main - Entry point                                                        *
+ * Return: returns 0 on succes
+ */
+
+int main(void)
+{
+        int n;
+
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+
+        if (n>0)
+        {
+                printf("%d is positive\n", n);
+        }
+        if (n<0)
+        {
+                printf("%d is negative\n", n);
+        }
+        if (n==0)
+        {
+                printf("%d is zero\n", n);
+        }
+
+	return(0);
+}
