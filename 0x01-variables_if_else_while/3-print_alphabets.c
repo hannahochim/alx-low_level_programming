c76924079359bca13d78d85024cdecad39824196
hannahochim <hannahochim@gmail.com>
2024-01-28 01:28:39 -0800

    Print alphabets both cases

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Contains main code.
+ *
+ * Description: This code prints the
+ * alphabet in lower and uppercase
+ * followed by a new line.
+ *
+ * Return: returns 0 on success.
+ */
+
+int main(void)
+{
+	char ch = 'a';
+	char CH = 'A';
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (CH = 'A'; CH <= 'Z'; CH++)
+		putchar(CH);
+	putchar('\n');
+
+	return (0);
+}
