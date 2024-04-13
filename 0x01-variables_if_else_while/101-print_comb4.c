713caa0411e36a63fb1c2131f173eddc162da9ed
hannahochim <hannahochim@gmail.com>
2024-04-13 13:23:34 -0700

    Print 3 digits combination

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - This contains main block
+ *
+ * Description: This program is designed to
+ * print all possible combination of three
+ * digit numbers.
+ *
+ * Return: returns 0 on success.
+ */
+
+int main(void)
+
+{
+	int r, s, t, u;
+
+	r = 0;
+	while (r < 800)
+	{
+		s = r / 100;
+		t = (r / 10) % 10;
+		u = r % 10;
+
+		if (s < t && t < u)
+		{
+			putchar(s + '0');
+			putchar(t + '0');
+			putchar(u + '0');
+
+			if (r < 800)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		r++;
+	}
+	putchar('\n');
+
+	return (0);
+}
