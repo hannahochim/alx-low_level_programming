69a7a3f8d6d9c27932cf5e841c146ce4f803b748
hannahochim <hannahochim@gmail.com>
2024-04-14 08:07:47 -0700

    Print the sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/** 
+ * print_sign - main block
+ *
+ * Description: Print the sign
+ * of a number
+ *
+ * Return: returns 1 or 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return(0);
+	}
+}
