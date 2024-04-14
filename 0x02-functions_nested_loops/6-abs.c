210720e64d588639d19aadaba596e915dd95027d
hannahochim <hannahochim@gmail.com>
2024-04-14 11:12:06 -0700

    Print absolute values

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs: Print absolute value
+ * of inputed number.
+ *
+ * Return: returns int value
+ * on success
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else if (n > 0)
+		return (n);
+	else
+		return (n);
+}
