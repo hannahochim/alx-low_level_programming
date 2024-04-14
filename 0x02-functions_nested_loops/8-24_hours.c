8e4802922f82c9eccc4b00f841d7a01c9618bcd8
hannahochim <hannahochim@gmail.com>
2024-04-14 12:39:49 -0700

    Print every minute

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print every minute
+ * of the day.
+ *
+ * Return: returns 0 on success
+ */
+
+void jack_bauer(void)
+{
+	int hour, min;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
