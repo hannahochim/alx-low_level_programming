e535314a384c0ba52dc72d0124ca96b085998e1c
hannahochim <hannahochim@gmail.com>
2024-04-14 04:52:39 -0700

    Print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - This contains code
+ *
+ * Description: Designed to print _putchar
+ *
+ * Return: returns 0 on success.
+ */
+
+int main (void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
