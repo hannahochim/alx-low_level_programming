af8e03d1d02c0f89d88697934561edc5c53135a7
hannahochim <hannahochim@gmail.com>
2024-04-14 07:08:09 -0700

    Print the alphabets 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * print_alphabet - Contains block of code
+ * print_alphabet_x10 - Contains block of code
  *
  * Description: This is designed to
  * print the alphabets in lower case
+ * ten times
  *
  * Return: returns 0 on success
  */
