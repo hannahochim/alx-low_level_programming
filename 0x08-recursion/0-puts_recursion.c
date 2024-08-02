4ad269d90c45c8b09bc44386e8cd90e0a6a58d58
hannahochim <hannahochim@gmail.com>
2024-08-02 05:56:06 -0700

    Print string and new line

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints a string
+ * and a new line
+ * @s: variable to print strings from
+ *
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
