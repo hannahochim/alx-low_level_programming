ac672b87c6c4dbbb714664cfd6780f26430cdaaa
hannahochim <hannahochim@gmail.com>
2024-08-02 12:05:37 -0700

    Free 2 dim grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,25 @@
+#include <main.h>
+#include <stdlib.h>
+
+/**
+* free_grid - frees up a 2d grid created in prev project
+* @grid: a pointer to a pointer. a 2d array grid
+* @height: height
+*
+* Description: this part should return the all clear from valgrind
+* Return: none it is void:
+*/
+
+void free_grid(int **grid, int height)
+{
+	int n = 0;
+
+	if (grid == NULL || height <= 0)
+		return;
+	while (n < height)
+	{
+		free(grid[n]);
+		n++;
+	}
+	free(grid);
+}
